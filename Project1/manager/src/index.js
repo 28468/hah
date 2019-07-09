e8352b0a3e5d0319d3e0915089969859cc108d25
jasonandjay <342690199@qq.com>
2019-07-09 11:49:40 +0800

    搭建dva架子

---
+++ Project1/manager/src/index.js
@@ -0,0 +1,20 @@
+import dva from 'dva';
+// 引入全局样式
+import './index.css';
+// 引入antd样式
+import 'antd/dist/antd.css';
+
+// 1. Initialize
+const app = dva();
+
+// 2. Plugins
+// app.use({});
+
+// 3. Model
+// app.model(require('./models/example').default);
+
+// 4. Router
+app.router(require('./router').default);
+
+// 5. Start
+app.start('#root');
